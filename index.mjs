e611ef37b2d3345a9d0ef50b2789ba04a22c4278
Francisco Rafart <francisco.rafart@gmail.com>
2022-01-17 10:43:48 -0500

    Add money bet to game

--- index.mjs
+++ index.mjs
@@ -2,12 +2,17 @@ import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs'; // backend that reach compile will produce
 const stdlib = loadStdlib(process.env);
 
+/// NOTE: Continue at setting Alice's wager
 (async () => {
     const startingBalance = stdlib.parseCurrency(100);
-    
     // These endowments only work on the Reach testing network
     const accAlice = await stdlib.newTestAccount(startingBalance);
     const accBob = await stdlib.newTestAccount(startingBalance);
+
+    const fmt = (x) => stdlib.formatCurrency(x, 4);
+    const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+    const beforeAlice = await getBalance(accAlice);
+    const beforeBob = await getBalance(accBob);
 
     const ctcAlice = accAlice.contract(backend); // deploy application for Alice
     const ctcBob = accBob.contract(backend, ctcAlice.getInfo()); // Attach Bob to application
@@ -32,9 +37,19 @@ const stdlib = loadStdlib(process.env);
     await Promise.all([
         ctcAlice.p.Alice({
             ...Player('Alice'),
+            wager: stdlib.parseCurrency(5),
         }),
         ctcBob.p.Bob({
             ...Player('Bob'),
+            acceptWager: (amt) => {
+                console.log(`Bob accepts the wager of ${fmt(amt)}`);
+            }
         }),
-    ])
+    ]);
+
+    const afterAlice = await getBalance(accAlice);
+    const afterBob = await getBalance(accBob);
+
+    console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
+    console.log(`Bob went from ${beforeBob} to ${afterBob}`);
 })();
--- index.rsh
+++ index.rsh
@@ -8,27 +8,36 @@ const Player = {
 export const main = Reach.App(() => {
     const Alice = Participant('Alice', {
         ...Player, // 'intract' will be bound to the methods defined in this object
+        wager: UInt,
     });
 
     const Bob = Participant('Bob', {
         ...Player,
+        acceptWager: Fun([UInt], Null),
     });
     init();
 
     Alice.only(() => { // Only Alice performs
+        const wager = declassify(interact.wager);
         const handAlice = declassify(interact.getHand()); // Calls front end function
     });
 
-    Alice.publish(handAlice); // Alice joins the application, the consensus network
+    Alice.publish(wager, handAlice).pay(wager); // Alice joins the application, the consensus network
     commit();
 
     Bob.only(() => {
+        interact.acceptWager(wager);
         const handBob = declassify(interact.getHand());
     })
 
-    Bob.publish(handBob);
+    Bob.publish(handBob).pay(wager);
+
     // Calculate the outcome before commiting
     const outcome = (handAlice+ (4 - handBob)) % 3;
+    const [forAlice, forBob] = outcome === 2 ? [2, 0] : outcome == 0 ? [0, 2] : [1, 1];
+
+    transfer(forAlice * wager).to(Alice);
+    transfer(forBob * wager).to(Bob);
 
     commit();
 
